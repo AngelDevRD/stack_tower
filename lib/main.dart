59f8d60b0448daf97894e10f0d8d640c164ebc48
Angel <angeldnielgs@gmail.com>
2026-09-17 02:27:41 -0400

    chore: probar release automatico

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 
+// Punto de entrada de la app.
 void main() {
   runApp(const ProviderScope(child: StackTowerApp()));
 }
